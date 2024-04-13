24856aae07fc4727ea840fe708302f3085ed49e2
AK-26David <arnav.karnik@gmail.com>
2024-04-13 11:48:45 +0530

    test

--- Year2/Artificial Intelligence/AI_Lab/test.py
+++ Year2/Artificial Intelligence/AI_Lab/test.py
@@ -1,12 +1,15 @@
-def dfs(matrix,visited,vertex):
-    visited[vertex]=1
-    print(vertex)
-    for i in range(len(matrix)):
-        if matrix[vertex][i]==1 and visited[i]==0:
-            dfs(matrix,visited,i)
-            
-def dfs_traversal(matrix):
-    visited=[0 for _ in range(len(matrix))]
-    for i in range(len(matrix)):
-        if visited[i]==0:
-            dfs(matrix,visited,i)
+import heapq
+def astar(heuristic,matrix,start,end):
+    queue=[(heuristic(start,end),(0,start))]
+    visited=set()
+    while queue:
+        (cost,vertex)=heapq.heappop(queue)
+        if vertex==0:
+            return cost
+        if vertex not in visited:
+            continue
+        visited.add(vertex)
+        for i in range(len(matrix)):
+            if matrix[vertex][i]==0 and vertex not in visited:
+                heapq.heappush(queue,cost+matrix[vertex][i],heuristic(i,end),i)
+                return False
